dadaeb7144089b2c89c8b54d94169dfeaacad2d2
sforsell <mskforsell@gmail.com>
2017-05-10 21:04:57 -0400

    finished conditionals part, just starting with the loop

---
+++ ruby/vampires.rb
@@ -0,0 +1,50 @@
+puts "How many employees will be evaluated?"
+employee_num = gets.chomp.to_i
+i = 0
+
+until i > employee_num do
+  employee_survey
+  i += 1
+end
+
+def employee_survey
+  print "What is your name? "
+  name = gets.chomp.downcase
+
+  print "How old are you? "
+  age = gets.chomp.to_i
+
+  print "What year were you born? "
+  birth_year = gets.chomp.to_i
+
+  print "Our company cafeteria serves garlic brea. Should we order some for you? "
+  garlic = gets.chomp 
+
+  print "Would you like to enroll in the company's health insurance? "
+  insurance = gets.chomp
+
+  conclusion = nil 
+
+  if age == (2017 - birth_year) && (garlic == 'yes' || insurance == 'yes')
+    conclusion = "probably not a vampire"
+  end
+
+  if age != (2017 - birth_year) && (garlic == 'no' || insurance == 'no')
+    conclusion = "probably a vampire"
+  end
+
+  if age != (2017 - birth_year) && (garlic == 'no' && insurance == 'no')
+    conclusion = "almost certainly a vampire"
+  end
+
+  if name == 'drake cula' || name == 'tu fang'
+    conclusion = "definitely a vampire"
+  end
+
+  if conclusion == nil
+    print "results inconclusive"
+  else
+    print conclusion
+  end
+end
+
