761e43601800d7d6102e6cb25e3496e4aa2a2dfb
sforsell <mskforsell@gmail.com>
2017-05-11 12:12:43 -0400

    set the initial allergy value to empty string

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,21 +1,21 @@
 def employee_survey
-  print "What is your name? "
+  print "- What is your name? "
   name = gets.chomp.downcase
 
-  print "How old are you? "
+  print "- How old are you? "
   age = gets.chomp.to_i
 
-  print "What year were you born? "
+  print "- What year were you born? "
   birth_year = gets.chomp.to_i
 
-  print "Our company cafeteria serves garlic brea. Should we order some for you? "
+  print "- Our company cafeteria serves garlic brea. Should we order some for you? "
   garlic = gets.chomp 
 
-  print "Would you like to enroll in the company's health insurance? "
+  print "- Would you like to enroll in the company's health insurance? "
   insurance = gets.chomp
 
-  p "Name any allergies you have, one at a time. When you're done or if you have none type 'done' "
-  allergy = gets.chomp
+  print "- Name any allergies you have, one at a time. When you're done or if you have none type 'done' "
+  allergy = "" # first input wont be checked if it's gets.chomp
 
 
   conclusion = nil 
@@ -36,24 +36,17 @@ def employee_survey
     conclusion = "#{name} is definitely a vampire "
   end
 
-  #until allergy == 'sunshine' || 'done'
-    
-  #  if allergy == 'sunshine'
-  #    conclusion = "#{name} is probably a vampire "
-  #  elsif allergy == 'done'
-  #    conclusion == conclusion
-  #  end
-
-  #end
-
   while allergy = gets.chomp
+
     if allergy == 'sunshine'
       conclusion = "#{name} is probably a vampire "
       break
     end
+
     if allergy == 'done'
       break
     end
+
   end
 
 
@@ -63,7 +56,7 @@ def employee_survey
     puts conclusion
   end
 
-
+  puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
         
 end
 
@@ -75,3 +68,8 @@ until i == employee_num do
   employee_survey
   i += 1
 end
+
+
+
+
+
