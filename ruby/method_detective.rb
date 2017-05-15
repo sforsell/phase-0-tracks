107a4fda5a444bfb0d628d3b21a7dfeca7c34932
sforsell <mskforsell@gmail.com>
2017-05-14 20:46:37 -0400

    swapper the <???> with method calls

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,43 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(2, "o")
 # => “zoom”
 
-# "enhance".<???>
+"enhance".center(15)
+"enhance".rjust(11).ljust(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+
+"the usual" << " suspects"
+"the usual".concat( " suspects")
+"the usual".insert(-1, " suspects" )
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".prepend("the usual")
+" suspects".insert(0, "the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chop
+"The case of the disappearing last letter".chomp("r")
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".slice(1..40)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".ord
 # => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+# (What is the significance of the number 122 in relation to the character z?) 
+# assuming it's used for keyboard recognition...?
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
