591705729bfeab8464691f4647ae156976f70c07
sforsell <mskforsell@gmail.com>
2017-05-16 12:39:07 -0400

    created interior design hash

---
+++ ruby/interior_design.rb
@@ -0,0 +1,11 @@
+# INTERIOR DESIGNER JOB APPLICATION
+application {
+  name: "Sarah Designer",
+  address: "123 fordham rd, fake city, FS",
+  email: "fake@fakemail.com",
+  phone: "555-555 5555"
+  fave_blue: "navy",
+  wallpaper_pref: ["Paisley", "Chevrons"],
+  ombre: "So last season"
+}
+
