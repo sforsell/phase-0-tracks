f26346c5354cc67a948474cc0e066a1402071886
sforsell <mskforsell@gmail.com>
2017-05-17 13:38:40 -0400

    added user interface and a while loop

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,38 +1,53 @@
 # separate first and last name
 # flip the order of the names
 # join together again
-# separate into individual character
-# compare each letter
+# separate into individual characters
+    # if letter is vowel, make it the next vowel
+        # unless it's the last vowel, then make it the first vowel.
+    # if the letter is a consonant make it the next consonant
+        # unless it's the last consonant, then make it the next consonant.
+    # if it's a space do nothing.
+# once you've changed the characters, join the name back together again.
 
+def alias_maker(full_name)
+  name = full_name.downcase.split(' ').reverse.join(' ').split('')
 
-name = "Sofia Forsell".downcase.split(' ').reverse.join(' ').split('')
-
-vowels = "aeiou"
-consonants = "bcdfghjklmnpqrstvwxyz"
-
-
-
-
-
-
-
+  vowels = "aeiou"
+  consonants = "bcdfghjklmnpqrstvwxyz"
 # =begin
-name.map! do |char|
-  if char == 'u'
-    char = 'a'
-  elsif char == 'z'
-    char = 'b'
-  elsif char == ' '
-    char = ' '
-  elsif consonants.index(char)
-    p consonants[consonants.index(char) + 1]
-  else vowels.index(char)
-    p vowels[vowels.index(char) + 1]
+  name.map! do |char|
+
+    if char == 'u'
+      char = 'a'
+    elsif char == 'z'
+      char = 'b'
+    elsif char == ' '
+      char = ' '
+    elsif consonants.index(char)
+      consonants[consonants.index(char) + 1]
+    else vowels.index(char)
+      vowels[vowels.index(char) + 1]
+    end
+
   end
+
+  p name.join
+
 end
 
-p name.join
 
+puts "type in your full name"
+original_name = ""
+
+while original_name = gets.chomp
+
+  if original_name == 'quit'
+    break
+  else
+    alias_maker(original_name)
+  end
+
+end
 
 
 
