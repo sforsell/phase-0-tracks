f64a7a1dfd7eb2fdd6639c58733911812f8ad27b
sforsell <mskforsell@gmail.com>
2017-05-20 21:15:24 -0400

    finished the hash part

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -68,8 +68,8 @@ other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
                             "solar battery", "flashlight"]
 # ----
 combined_supplies = other_survivor_supplies.push(my_supplies).flatten.uniq!
-
 p combined_supplies
+
 # Hash Drills
 
 extinct_animals = {
@@ -85,17 +85,31 @@ extinct_animals = {
 # 1. Iterate through extinct_animals hash, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
 # ----
+extinct_animals.each_pair { |species, year| print "#{species}-#{year}*"}
 
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
 # ----
+=begin
+extinct_animals2 = {}
+
+extinct_animals.each do |key, value|
+	if value < 2000 
+		extinct_animals2[key] = value
+	end
+end
+extinct_animals = extinct_animals2
+p extinct_animals
+=end
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # so they accurately reflect what year the animal went extinct.
 # Do not use any special built-in methods.
 # ----
+extinct_animals.each { |species, year| extinct_animals[species] = year-3 }
 
+p extinct_animals.values
 # 4. You've heard that the following animals might be extinct, but you're not sure.
 # Check if they're included in extinct_animals, one by one:
 # "Andean Cat"
@@ -103,9 +117,24 @@ extinct_animals = {
 # "Saiga Antelope"
 # Do not use any special built-in methods.
 # ----
-
+extinct_animals.each do |keys, values|
+	if keys == "Andean Cat"
+		print "Andean Cat is a match"
+	elsif keys == "Dodo"
+		print "Dodo is a match"
+	elsif keys == "Saiga Antelope"
+		print "Saiga Antelope is a match"
+	end
+end
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find the built-in method that helps you accomplish this in the Ruby documentation
 # for Hashes.
 # ----
+new_array =  [ "Passenger Pigeon", extinct_animals.delete("Passenger Pigeon") ]
+# OR
+# p extinct_animals.assoc("Passenger Pigeon")
+# extinct_animals.delete("Passenger Pigeon")
+
+p extinct_animals
+p new_array
