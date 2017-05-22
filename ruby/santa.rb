25456ed0fa76e351602992666e74c9e2c357108e
sforsell <mskforsell@gmail.com>
2017-05-22 10:22:33 -0400

    finished release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -15,6 +15,31 @@ class Santa
 		cookie_type
 	end
 
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer)
+		@reindeer_ranking.slice!(@reindeer_ranking.index(reindeer))
+		@reindeer_ranking.push(reindeer)
+	end	
+
+#getter
+
+	def age 
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+#setter
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
 end
 
 santas = []
@@ -23,9 +48,30 @@ genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N
 	"prefer not to say", "basic bitch", "priviliged", "it's complicated"]
 ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", 
 	"unicorn", "N/A", "ginger", "faun", "centaur", "mermaid"]
-
+=begin
 	genders.length.times do |i|
 		santas << Santa.new(genders[i], ethnicities[i])
 	end
+=end
 
-	print santas
+print santas
+
+santa1 = Santa.new("male", "white")
+p santa1
+
+santa1.get_mad_at("Rudolph")
+santa1.celebrate_birthday
+p santa1.age
+
+p santa1
+
+santa1.gender = "transforming"
+
+p santa1
+
+
+
+
+
+
+
