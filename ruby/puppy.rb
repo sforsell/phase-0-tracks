c6d5876b52b2e6d0f4e448a333fb0e376a841749
sforsell <mskforsell@gmail.com>
2017-05-21 21:35:01 -0400

    designed my puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+# The Puppy Class
+# puppy example
+
+
+
+# Characteristics
+	# Breed: varies
+  # eyes: 2
+  # legs: 4
+  # tail: yes
+  # fur: varies
+  # ears: up/down
+  # cuteness level: varies.
+
+# Behaviour
+	# Jumping
+	# chewing
+	# barking
+	# napping
+	# playing
