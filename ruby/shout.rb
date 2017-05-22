30e6e84217924aca115a999b78072d90f4bb92b1
sforsell <mskforsell@gmail.com>
2017-05-22 12:10:05 -0400

    changed the module to be a mix-in instead of a standalone

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,5 @@
+=begin
+
 module Shout
 	def self.yell_angrily(words)
 		words.upcase + "!!!" + " :( "
@@ -7,3 +9,33 @@ module Shout
 		words + "!!" + " :D "
 	end
 end
+
+=end
+
+module Shout
+	def yell_angrily(words)
+		words.upcase + "!!! :( "
+	end
+
+	def yell_happily(words)
+		words + "!! :D "
+	end
+end
+
+class OldPerson
+	include Shout
+end
+
+class Kid
+	include Shout
+end
+
+old_man_jim = OldPerson.new
+
+p old_man_jim.yell_angrily("get off my property")
+p old_man_jim.yell_happily("Look, it's my grandchild")
+
+ella = Kid.new
+
+p ella.yell_happily("Ice cream")
+p ella.yell_angrily("I want ice cream") 
