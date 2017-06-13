ecba888716a6643f262bdf780aa4d4d015d9551e
sforsell <mskforsell@gmail.com>
2017-06-13 12:47:54 -0400

    made an html sample page and a js file with some jquery calls.

---
+++ web_dev/jquery/jquery.js
@@ -0,0 +1,46 @@
+/* need this in html head
+<script src="https://code.jquery.com/jquery-1.12.4.js"> 
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"> 
+*/
+
+$( "#button" ).on( "click", function() {
+      $( "#effect" ).toggle( "fade", 500 );
+    });
+
+ // Toggle effects: blind, bounce, clip, drop, explode, fade, 
+ // fold, highlight, puff, pulsate, scale, shake, size, slide
+ // if you don't want to bring element back and forth there's 
+ // also hide and show. 
+
+
+$( "#button2" ).on( "click", function() {
+   $( "#otherEffect" ).animate({
+          backgroundColor: "#aa0000",
+          color: "#fff",
+          width: 500
+        }, 1000 );
+
+});
+
+$( "#draggable" ).draggable();
+
+// draggable has a bunch of extras, like clone, opacity, snap, 
+// zindex, distance and more. 
+
+$("#sortable").sortable();
+
+// sortable also has a ton of extra options, methods and events. 
+
+/*
+.uniqueId()
+
+.uniqueId() will check if the element has an id, and if not, 
+it will generate one and set it on the element. It is safe to call .uniqueId() 
+on an element without checking if it already has an id. 
+If/when the widget needs to clean up after itself, the .removeUniqueId() 
+method will remove the id from the element if it was added by .uniqueId() and 
+leave the id alone if it was not. .removeUniqueId() is able to be smart about 
+this because the generated ids have a prefix of "ui-id-".
+*/
+
+$( "#tabs" ).tabs();
